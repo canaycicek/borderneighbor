faed5e0b6bc284dc0a801743dd526b8c7b6a6067
canaycicek <can.m.aycicek@gmail.com>
2023-10-21 17:06:42 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function displayCountry(country) {
-  const request = new XMLHttpRequest();
+const txtSearch = document.getElementById("txtSearch");
+const btnSearch = document.getElementById("btnSearch");
+const countrys = document.getElementById("countrys");
+const btnSend = document.getElementById("btnSend");
 
+btnSearch.addEventListener("click", function () {
+  let text = txtSearch.value;
+  getCountry(text);
+  txtSearch.value = "";
+});
+
+function getCountry(country) {
+  const request = new XMLHttpRequest();
   request.open("GET", "https://restcountries.com/v3.1/name/" + country);
   request.send();
 
@@ -8,49 +18,80 @@ function displayCountry(country) {
 
   request.addEventListener("load", function () {
     const data = JSON.parse(this.responseText);
-    setCountry(data);
+    renderCountry(data[0]);
 
     const countries = data[0].borders.toString();
 
-    // load neighbor
+    // load neighbors
     const req = new XMLHttpRequest();
+
     req.open("GET", "https://restcountries.com/v3.1/alpha?codes=" + countries);
     req.send();
 
     req.addEventListener("load", function () {
       const data = JSON.parse(this.responseText);
-      console.log(data);
-      console.log(typeof data);
-      setCountry(data);
+      renderNeighbors(data);
     });
   });
 }
 
-function setCountry(data) {
-  data.forEach((country) => {
-    const html = `
-                <div class="col-3">
-                    <div class="card h-100">
-                        <img src="${country.flags.png}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">${country.name.common}</h5>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Population: ${(
-                              country.population / 1000000
-                            ).toFixed(1)}</li>
-                            <li class="list-group-item">Capital: ${
-                              country.capital
-                            }</li>
-                            <li class="list-group-item">Language: ${Object.values(
-                              country.languages
-                            )}</li>
-                        </ul>
-                    </div>
-                </div>
-            `;
-    document.querySelector(".row").insertAdjacentHTML("beforeend", html);
-  });
+function renderCountry(data) {
+  let html = `
+    <div class="card-header">
+      Arama Sonucu
+    </div>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-4">
+          <img src="${data.flags.png}" class="img-fluid">
+        </div>
+        <div class="col-8">
+          <h3 class="card-title">${data.name.common}</h3>
+          <hr>
+          <div class="row">
+            <div class="col-2">Nüfus: </div>
+            <div class="col-10">${(data.population / 1000000).toFixed(1)} M
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-2">Resmi Dil: </div>
+            <div class="col-10">${Object.values(data.languages)}
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-2">Başkent: </div>
+            <div class="col-10">${data.capital}
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-2">Para Birimi: </div>
+            <div class="col-10">${Object.values(data.currencies)[0].name} (${
+    Object.values(data.currencies)[0].symbol
+  })
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  document.querySelector("#country-details").innerHTML = html;
 }
 
-displayCountry("germany");
+function renderNeighbors(data) {
+  console.log(data);
+  let html = "";
+  data.forEach((country) => {
+    html += `
+      <div class="col-2 mt-2">
+        <div class="card">
+          <img src="${country.flags.png}" class="card-img-top">
+          <div class="card-body">
+            <h6 class="card-title">${country.name.common}</h6>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  document.querySelector("#neighbors").innerHTML = html;
+}
